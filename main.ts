51469c39e28ea98cf7184591fa7e676d0348d35d
Taher Shabbir <taherihh53@gmail.com>
2024-06-09 03:02:48 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,28 @@
+//storing a person name in a variable 
+
+//let personName:string = "Ambreen Taher"
+
+//printing a person name in lowercase
+
+//console.log("Lowercase:", personName.toLowerCase());
+
+//printing a person name in uppercase
+
+//console.log("Uppercase:", personName.toUpperCase());
+
+//titlecase
+let personName:string = "i am learning typescript"
+//step 01
+let words:string[] = personName.split(" ");
+
+//step 02
+let titleCaseName: string = "";
+
+//step 03
+//for loop 1-10
+for (let i = 0; i < words.length; i++) {
+   titleCaseName += words[i].charAt(0).toUpperCase()
+    + words[i].slice(1).toLowerCase() + " "
+    
+}
+console.log(titleCaseName);
